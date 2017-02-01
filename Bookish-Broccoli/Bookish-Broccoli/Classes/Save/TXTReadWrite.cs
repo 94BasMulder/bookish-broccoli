be65897b8d7b9092f342cc7a0e6bd113d3cfff94
94BasMulder <bassie780@gmail.com>
2017-02-01 23:31:48 +0100

    Start up
    First commit
    Project and Initial classes and naming convention.txt

---
+++ Bookish-Broccoli/Bookish-Broccoli/Classes/Save/TXTReadWrite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookish_Broccoli.Classes.Save
+{
+    class TXTReadWrite
+    {
+    }
+}
